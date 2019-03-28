a5c5c97f60a43f7878dd2d32cc2343aef6447470
RinatTr <rinatreg@gmail.com>
2019-03-27 20:12:03 -0400

    distance functionality

--- frontend/src/components/Home/home.js
+++ frontend/src/components/Home/home.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 import Map from './map.js';
 import SideBar from './sidebar.js';
 
+import '../../css/Home.css';
+
 export default class Home extends Component {
   constructor(){
     super()
@@ -11,7 +13,8 @@ export default class Home extends Component {
       markers: [],
       latitude: "",
       longitude: "",
-      userIP: ""
+      userIP: "",
+      distances: []
     }
   }
 
@@ -22,13 +25,13 @@ export default class Home extends Component {
          this.setState({
            userIP: res.data.ip
          });
-         console.log("ip: ", res.data.ip);
+         // console.log("ip: ", res.data.ip);
        })
        .then(() => {
          let apikey = "45c642a38d6e8f01dac07ba1f003505a";
          axios.get(`http://api.ipstack.com/${this.state.userIP}?access_key=${apikey}`)
                  .then(res => {
-                   console.log("result long/ lat!!!: ", res)
+                   // console.log("result long/ lat!!!: ", res)
                    this.setState({
                      latitude: res.data.latitude,
                      longitude: res.data.longitude
@@ -36,21 +39,33 @@ export default class Home extends Component {
                  })
                  .then(() => {
                         this.fetchLocation()
+                        .then(() => {
+                              this.getDistanceData(this.coordinateString())
+                            })
                       })
        })
        .catch(err => {
          console.log(err)
        })
   }
-
+  getDistanceData = (string) => {
+    let {longitude, latitude} = this.state;
+     axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${latitude},${longitude}&destinations=${string}&key=AIzaSyAm8VcTZZ0P2oJCVLZ4ZDy5RK2UYxMxDlc`)
+     .then(res => {
+       let normalizeDistance = res.data.rows[0].elements.map(el => {return el = el.distance.text})
+       this.setState({
+          distances: normalizeDistance
+        });
+     })
+   }
   fetchLocation = () => {
-    console.log("fetchLocation!!!");
-    axios
+    // console.log("fetchLocation!!!");
+    return axios
       .get(
         `https://www.hikingproject.com/data/get-trails?lat=${this.state.latitude}&lon=${this.state.longitude}&maxDistance=10&key=200430061-384fefbb8ceed621af7cea7e5ab597b2`
       )
       .then(res => {
-        console.log("Res Hiking!!!!", res.data.trails);
+        // console.log("Res Hiking!!!!", res.data.trails);
         this.setState({
           markers: res.data.trails
         });
@@ -59,14 +74,17 @@ export default class Home extends Component {
         console.log(err)
       })
   };
+  coordinateString = () => {
+    return this.state.markers.map(el=> {return el.latitude+"%2C"+el.longitude+"%7C"}).join('').slice(0,-3)
+    // console.log("coorStr=>",output,this.state.markers);
+  }
 
   render() {
-    const {markers, latitude, longitude, userIP} = this.state
+    const {markers, latitude, longitude, userIP, distances} = this.state
     return (
       <React.Fragment>
-        <div className="main-container">
-          <h1>Home</h1>
-          <SideBar props={this.state.markers}/>
+        <div className="home-main-container">
+          <SideBar distances={distances} trails={markers} currentLon={longitude} currentLat={latitude}/>
           <Map markers={markers} latitude={latitude} longitude={longitude} userIP={userIP}/>
         </div>
 
--- frontend/src/components/Home/sidebar.js
+++ frontend/src/components/Home/sidebar.js
@@ -2,15 +2,30 @@ import React from 'react';
 import TrailTile from './trailTile.js';
 import { Link } from 'react-router-dom';
 
-const SideBar = ({props}) => {
-  console.log("inside sidebar=>",props);
-  let mapTiles;
+const SideBar = ({trails, currentLon, currentLat, distances}) => {
+  console.log("inside sidebar =>", trails);
+  //dummy data
+
+  let mapTiles = trails.map((trail,i) => {
+    return <Link key={i} to={`/trail/${trail.id}`}>
+              <TrailTile
+                currentLon={currentLon}
+                currentLat={currentLat}
+                difficulty={trail.difficulty}
+                length={trail.length}
+                imgSqSmall={trail.imgSqSmall}
+                id={trail.id}
+                distance={distances[i]}
+              />
+            </Link>
+  });
   return (
     <div className="sidebar-container">
-      <h1>sidebar</h1>
-      <TrailTile/>
+      {mapTiles}
     </div>
   )
 }
 
 export default SideBar;
+    // distance={normalizeDistance[i]}
+    // let normalizeDistance = response.rows[0].elements.map(el => {return el = el.distance.text})
